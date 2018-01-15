5487b35e6abb3695cc2792f822c9becc410ddad5
psychobolt <psychobolt@gmail.com>
2018-01-15 11:28:18 -0800

    0.0.46

--- dist/index.js
+++ dist/index.js
@@ -1,5 +1,7 @@
 if (process.env.NODE_ENV === 'development') {
+  window.DEBUG = true;
   module.exports = require('./index.dev');
 } else {
+  window.DEBUG = false;
   module.exports = require('./index.prod');
 }
